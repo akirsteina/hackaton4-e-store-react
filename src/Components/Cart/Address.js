500cfee97f23e0602aaea689dcdc6b84fad07257
akirsteina <akirsteina@users.noreply.github.com>
2021-07-12 12:18:15 +0300

    update attribute name

--- src/Components/Cart/Address.js
+++ src/Components/Cart/Address.js
@@ -7,7 +7,7 @@ function Adress({ billingAddress, setBillingAddress, shippingAddress, setShippin
         <div className="row my-5">
             <div className="col">
                 <div className="mb-3">
-                    <label for="shipping-address" className="form-label">Shipping address:</label>
+                    <label htmlFor="shipping-address" className="form-label">Shipping address:</label>
                     <textarea className="form-control address" id="shipping-address" rows="7" value={shippingAddress} onChange={(event) => { setShippingAddress(event.target.value) }} />
                 </div>
                 <div className="form-check">
